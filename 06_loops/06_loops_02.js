2d99ee8fe3cb65e3ef19b02aa179c7c124c012dc
hemie143 <hemie143@gmail.com>
2022-08-29 22:31:19 +0200

    Chapter 7 Page 3

---
+++ 06_loops/06_loops_02.js
@@ -0,0 +1,9 @@
+// Write your code below
+let vacationSpots = ['Barcelona', 'Paris', 'Berlin'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+
+
+
---
+++ 07_iterators/07_iter_02.js
@@ -0,0 +1,4 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(f => console.log('I want to eat a ' + f));
